af21b4d6556da1b5ebb0e247eaaabff57731aead
Aufaajie <a710230020@student.ums.ac.id>
2024-06-07 18:08:37 +0700

    Create hari2.py

---
+++ Day 2/hari2.py
@@ -0,0 +1,7 @@
+angka_1 = input('Masukan angka 1 :')
+angka_2 = input('Masukan angka 2 :')
+
+hasil_bagi = float(angka_1) / float(angka_2)
+hasil_modulus = float(angka_1) % float(angka_2)
+print('Hasil angka 1 dibagi angka 2 = ', hasil_bagi)
+print('Hasil modulus angka 1 dengan angka 2 =', hasil_modulus)
