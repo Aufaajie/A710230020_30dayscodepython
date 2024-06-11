d730238aa775b099198a5e9ceb6daa371f5f1b8b
Aufaajie <a710230020@student.ums.ac.id>
2024-06-11 17:13:57 +0700

    Create hari6.py

---
+++ Day 6/hari6.py
@@ -0,0 +1,10 @@
+# Program untuk mengonversi suhu dari Celsius ke Fahrenheit
+
+# Meminta input suhu dalam Celsius dari pengguna
+suhu_celsius = float(input("Masukkan suhu dalam Celsius: "))
+
+# Mengonversi suhu dari Celsius ke Fahrenheit
+suhu_fahrenheit = (suhu_celsius * 9/5) + 32
+
+# Menampilkan hasil konversi
+print("Suhu dalam Fahrenheit:", suhu_fahrenheit)
